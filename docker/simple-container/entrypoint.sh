1e05f43e74faacc692f410a42e4aa15cae35d46b
Flosch62 <schwarz.flori.88@gmail.com>
2025-04-26 11:39:01 +0200

    remove redundant group existence checks in entrypoint script

--- docker/simple-container/entrypoint.sh
+++ docker/simple-container/entrypoint.sh
@@ -6,20 +6,6 @@ API_GROUP=${API_USER_GROUP:-clab_api}
 ADMIN_GROUP=${SUPERUSER_GROUP:-clab_admins}
 REQUIRED_LOGIN_GROUP="clab_admins"
 
-echo "Ensuring internal groups exist: $REQUIRED_LOGIN_GROUP, $API_GROUP, $ADMIN_GROUP"
-if ! getent group "$REQUIRED_LOGIN_GROUP" > /dev/null; then 
-  addgroup --system "$REQUIRED_LOGIN_GROUP" || addgroup "$REQUIRED_LOGIN_GROUP"
-  echo "INFO: Ensured group $REQUIRED_LOGIN_GROUP exists"
-fi
-if ! getent group "$API_GROUP" > /dev/null; then 
-  addgroup --system "$API_GROUP" || addgroup "$API_GROUP"
-  echo "INFO: Ensured group $API_GROUP exists"
-fi
-if ! getent group "$ADMIN_GROUP" > /dev/null; then 
-  addgroup --system "$ADMIN_GROUP" || addgroup "$ADMIN_GROUP"
-  echo "INFO: Ensured group $ADMIN_GROUP exists"
-fi
-
 # Setup labs directory
 SHARED_LABS_DIR=${CLAB_SHARED_LABS_DIR:-/opt/containerlab/labs}
 echo "Setting up labs directory: $SHARED_LABS_DIR"
